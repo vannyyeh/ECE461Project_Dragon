b7f986620c88fd3debaf577c1a6fbe0c264c652b
Milton Nava <60264699+mjn874@users.noreply.github.com>
2023-03-25 16:05:12 -0500

    page for user to see their different hardware projects
    uploaded my code from homework 5 to begin using as the template for the page where users can check in or check out hardware and join or leave projects

---
+++ frontend/src/user_hardware_mgt_page/App.js
@@ -0,0 +1,16 @@
+import { Button, TextField } from '@mui/material';
+import logo from './logo.svg';
+import './App.css';
+import Projects from './Projects'
+import React, { Component } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Projects/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/user_hardware_mgt_page/ProjectTab.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './ProjectTab.css';
+import { TextField, Button } from '@mui/material';
+
+function HWSet(props) {
+  const [numerator, setNumerator] = React.useState('0');
+  const [denominator, setDenominator] = React.useState('100');
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleCheckIn = () => {
+    const newValue = parseInt(numerator) - parseInt(inputValue);
+    setNumerator(newValue < 0 ? '0' : newValue.toString());
+    setInputValue('');
+  };
+
+  const handleCheckOut = () => {
+    const newValue = parseInt(numerator) + parseInt(inputValue);
+    setNumerator(newValue > parseInt(denominator) ? denominator : newValue.toString());
+    setInputValue('');
+  };
+
+  return (
+    <div className="hwset">
+      <div className="hwset-label">
+        <label className="label">HWSet {props.number}</label>
+        <span className="fraction">{numerator}/{denominator}</span>
+      </div>
+      <TextField
+        className="text-field"
+        label="Enter Quantity"
+        variant="outlined"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Button variant="contained" color="primary" onClick={handleCheckIn}>
+        Check In
+      </Button>
+      <Button variant="contained" color="secondary" onClick={handleCheckOut}>
+        Check Out
+      </Button>
+    </div>
+  );
+}
+
+
+
+function ProjectTab(props) {
+
+  const handleJoinClick = () => {
+    // function to handle Join button click
+    console.log(`Join clicked for project ${props.title}`);
+  };
+
+  return (
+    <div className="project-tab">
+      <div className="title">{props.title}</div>
+      <div className="description">{props.description}</div>
+      <div className="hwsets">
+        <HWSet number="1:" />
+        <HWSet number="2:" />
+      </div>
+      <Button variant="contained" color="primary" onClick={handleJoinClick}>
+        Join
+      </Button>
+    </div>
+  );
+}
+
+export default ProjectTab;
